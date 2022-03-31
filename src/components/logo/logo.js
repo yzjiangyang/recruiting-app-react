513b72bdc681601bc2f4b7c22050d1dacbac9e64
yzjiangyang <yzjiangyang123@163.com>
2022-03-31 15:03:31 -0700

    Revert "register and login page"
    This reverts commit 0ad071d026a9d265d0a9f581e05d9a9a01797e78.

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -2,10 +2,10 @@ import './logo.less'
 import logo from './logo.png'
 import React from 'react'
 
-export default function Logo(){
+export default function Logo() {
     return (
-        <div className="logo-containter">
-            <img src={ logo } alt='logo' className='logo-img' />
+        <div className='logo-container'>
+            <img src={logo} alt='logo' className='logo-img'/>
         </div>
     )
 }
