68ee24661cb21c81eed27c67a4829a5f67cb8f34
yzjiangyang <yzjiangyang123@163.com>
2022-03-31 15:15:36 -0700

    Revert "register and login page"
    This reverts commit 0ad071d026a9d265d0a9f581e05d9a9a01797e78.

--- src/containers/register/register.js
+++ src/containers/register/register.js
@@ -1,73 +1,9 @@
-import { NavBar, WingBlank, List, InputItem, WhiteSpace, Radio, Button } from 'antd-mobile'
-import Logo from "../../components/logo/logo"
-import React from 'react'
+import React from "react";
 
 class Register extends React.Component {
-    state = {
-        username: '',
-        password: '',
-        confirmPassword: '',
-        type: 'applicant'
-    }
-
-    handleChange = (name, val) => {
-        this.setState({
-            [name]: val
-        })
-    }
-
-    register = () => {
-        console.log(this.state)
-    }
-
-    toLogin = () => {
-        this.props.history.replace('/login')
-    }
-
     render() {
         return (
-            <div>
-                <NavBar>Silicon Valley</NavBar>
-                <Logo />
-                <WingBlank>
-                    <List>
-                        <WhiteSpace />
-                        <InputItem
-                            onChange={(val) => this.handleChange('username', val)}
-                        >Username:</InputItem>
-
-                        <WhiteSpace />
-                        <InputItem
-                            onChange={(val) => this.handleChange('password', val)} type="password"
-                        >Password</InputItem>
-                        <WhiteSpace />
-
-                        <InputItem
-                            onChange={(val) => this.handleChange('confirmPassword', val)} type="password"
-                        >Password</InputItem>
-
-                        <WhiteSpace />
-                        <List.Item>
-                            <span>User Type</span>
-                            &nbsp;&nbsp;&nbsp;
-                            <Radio
-                                checked={this.state.type==='applicant'}
-                                onChange={() => this.handleChange('type', 'applicant')}
-                            >Applicant</Radio>
-                            
-                            &nbsp;&nbsp;&nbsp;
-                            <Radio
-                                checked={this.state.type==='recruiter'}
-                                onChange={() => this.handleChange('type', 'recruiter')}
-                            >Recruiter</Radio>
-                        </List.Item>
-                        <WhiteSpace />
-                        <Button type="primary" onClick={this.register}>Signup</Button>
-                        <WhiteSpace />
-                        <Button onClick={this.toLogin}>Already registered</Button>
-                    </List>
-                </WingBlank>
-            </div>
+            <div>register</div>
         )
     }
 }
