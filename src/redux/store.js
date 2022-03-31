18c0188df21af23b5397de922449886c02d33a4d
yzjiangyang <yzjiangyang123@163.com>
2022-03-31 13:20:47 -0700

    use redux

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { createStore, applyMiddleware } from 'redux'
+import reducers from './reducers'
+import thunk from 'redux-thunk'
+
+
+export default createStore(
+    reducers,
+    composeWithDevTools(applyMiddleware(thunk))
+    )
