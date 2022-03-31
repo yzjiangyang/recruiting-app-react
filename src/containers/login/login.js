0ad071d026a9d265d0a9f581e05d9a9a01797e78
yzjiangyang <yzjiangyang123@163.com>
2022-03-31 15:01:56 -0700

    register and login page

--- src/containers/login/login.js
+++ src/containers/login/login.js
@@ -1,9 +1,51 @@
-import React from "react";
+import { NavBar, WingBlank, List, InputItem, WhiteSpace, Button } from 'antd-mobile'
+import Logo from "../../components/logo/logo"
+import React from 'react'
 
 class Login extends React.Component {
+    state = {
+        username: '',
+        password: ''
+    }
+
+    handleChange = (name, val) => {
+        this.setState({
+            [name]: val
+        })
+    }
+
+    login = () => {
+        console.log(this.state)
+    }
+
+    toRegister = () => {
+        this.props.history.replace('/register')
+    }
+
     render() {
         return (
-            <div>login</div>
+            <div>
+                <NavBar>Silicon Valley</NavBar>
+                <Logo />
+                <WingBlank>
+                    <List>
+                        <WhiteSpace />
+                        <InputItem
+                            onChange={(val) => this.handleChange('username', val)}
+                        >Username:</InputItem>
+
+                        <WhiteSpace />
+                        <InputItem
+                            onChange={(val) => this.handleChange('password', val)} type="password"
+                        >Password</InputItem>
+                        
+                        <WhiteSpace />
+                        <Button type="primary" onClick={this.login}>Login</Button>
+                        <WhiteSpace />
+                        <Button onClick={this.toRegister}>Not registered?</Button>
+                    </List>
+                </WingBlank>
+            </div>
         )
     }
 }
